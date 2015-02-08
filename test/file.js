a055e43a3d8eb78ec029f48de8bb76ee0e254053
bouzuya <m@bouzuya.net>
2015-02-08 20:42:32 +0900

    add File

---
+++ test/file.js
@@ -0,0 +1,17 @@
+var assert = require('power-assert');
+var sinon = require('sinon');
+var File = require('../lib/file').File;
+
+describe('File', function() {
+  beforeEach(function() {
+    this.sinon = sinon.sandbox.create();
+  });
+
+  afterEach(function() {
+    this.sinon.restore();
+  });
+
+  describe.skip('.exists', function() {});
+  describe.skip('.read', function() {});
+  describe.skip('.write', function() {});
+});
